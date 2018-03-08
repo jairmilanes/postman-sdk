45de904bf6b2cd9a0d7134c1c58867768029fd26
COBRA\COBRA <weblayoutz@gmail.com>
2018-03-07 19:37:42 -0800

    Fixed jsComments with proper types and description

--- src/index.js
+++ src/index.js
@@ -1,167 +1,170 @@
-import client from "request";
+import client from 'request'
+import collection from './lib/collection'
+import environment from './lib/environment'
 
 /**
  * Get the request options
  *
- * @param {String} method The request method
- * @param {String} target The target path
- * @param {Object} params The params object
+ * @param {string} method The request method
+ * @param {string} target The target path
+ * @param {object} params The params object
  * @param {*} id The optional request id
- * @returns {{host: string, method: string}} The request config
+ * @returns {object} The request config
  */
-const getOptions = (method, target, params, id) => ({
-  url: `/${target}${id ? `/${id}` : ""}`,
-  qs: method === "GET" ? params : {},
-  body: method !== "GET" ? params : {},
-  method,
-  baseUrl: "https://api.getpostman.com/",
-  json: true,
-  headers: {
-    "X-Api-Key": process.env.POSTMAN_API_KEY
-  }
-});
+const getOptions = (method, target, params, id) => {
+	return {
+		baseUrl: 'https://api.getpostman.com/',
+		uri: `/${target}${id ? `/${id}` : ''}`,
+		qs: method === 'GET' ? params : {},
+		body: method !== 'GET' ? params : {},
+		method,
+		json: true,
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Api-Key': process.env.POSTMAN_API_KEY
+		}
+	}
+}
 
 /**
  * Promise callback
  *
- * @param {Function} resolve The resolve callback
- * @param {Function} reject The reject callback
- * @returns {Function} A Promise callback
+ * @param {function} resolve The resolve callback
+ * @param {function} reject The reject callback
+ * @returns {function} A Promise callback
  */
-const callbackFactory = (resolve, reject) => {
-  /**
-   * Request callback
-   *
-   * @param {Object} error The error object
-   * @param {Object} response The response object
-   * @param {Object|Array} body The response body
-   * @returns {Object|Array} The response body
-   */
-  const callback = (error, response, body) => {
-    if (error) {
-      reject(error);
-    }
+const callbackFactory = (resolve, reject) =>
+	/**
+	 * Request callback
+	 *
+	 * @param {object} error The error object
+	 * @param {object} response The response object
+	 * @param {object|Array} body The response body
+	 * @returns {object|Array} The response body
+	 */
+	(error, response, body) => {
+		if (error) {
+			reject(error)
+		}
 
-    resolve(body);
-  };
-
-  return callback;
-};
+		resolve(body)
+	}
 
 /**
- * Creates a request with id
+ * Request factory
  *
- * @param {String} method The method name
- * @param {String} target The request target (eg: collections)
- * @returns {Function} The request function
+ * @param {string} method The method name
+ * @param {string} target The request target (eg: collections)
+ * @returns {function(id:string,params:object)} The request function
  */
 const requestFactory = (method, target) => {
-  /**
-   * Performs a request with an id parameter
-   *
-   * @param {*} id The optional request id
-   * @param {Object} params The params object
-   * @returns {Promise<any>} A promise
-   */
-  return (id = null, params = {}) => {
-    if (typeof id === "object") {
-      params = id;
-      id = null;
-    }
-    return new Promise((resolve, reject) => {
-      client[method.toLowerCase()](
-        getOptions(method, target, params, id),
-        callbackFactory(resolve, reject)
-      );
-    });
-  };
-};
+	/**
+	 * Performs a request with an id parameter
+	 *
+	 * @param {*} id The optional request id
+	 * @param {object} params The params object
+	 * @returns {Promise} A promise
+	 */
+	return (id = null, params = {}) => {
+		if (method.toLowerCase() === 'post') {
+			params = Object.assign({}, id)
+			id = null
+		}
+
+		return new Promise((resolve, reject) => {
+			client[method.toLowerCase()](
+				getOptions(method, target, params, id),
+				callbackFactory(resolve, reject)
+			)
+		})
+	}
+}
 
 /**
  * Prepares a post method
  *
- * @param {String} target The call target
- * @returns {Function} The request method
- * @private
+ * @param {string} target The call target
+ * @returns {function} The request method
  */
 const _post = target =>
-  /**
-   * Performs a post request
-   *
-   * @param {Object} params The params object
-   * @param {*} id The optional request id
-   * @returns {Promise<any>} A promise
-   */
-  requestFactory("POST", target);
+	/**
+	 * Performs a post request
+	 *
+	 * @param {*} id The optional request id
+	 * @param {object} params The params object
+	 * @returns {Promise} A promise
+	 */
+	requestFactory('POST', target)
 
 /**
  * Prepares a post method
  *
- * @param {String} target The call target
- * @returns {Function} The request method
+ * @param {string} target The call target
+ * @returns {function} The request method
  * @private
  */
 const _get = target =>
-  /**
-   * Performs a get request
-   *
-   * @param {Object} params The params object
-   * @param {*} id The optional request id
-   * @returns {Promise<any>} A promise
-   */
-  requestFactory("GET", target);
+	/**
+	 * Performs a get request
+	 *
+	 * @param {*} id The optional request id
+	 * @param {object} params The params object
+	 * @returns {Promise} A promise
+	 */
+	requestFactory('GET', target)
 
 /**
  * Prepares a post method
  *
- * @param {String} target The call target
- * @returns {Function} The request method
+ * @param {string} target The call target
+ * @returns {function} The request method
  * @private
  */
 const _put = target =>
-  /**
-   * Performs a put request
-   *
-   * @param {Object} params The params object
-   * @param {*} id The optional request id
-   * @returns {Promise<any>} A promise
-   */
-  requestFactory("PUT", target);
+	/**
+	 * Performs a put request
+	 *
+	 * @param {*} id The optional request id
+	 * @param {object} params The params object
+	 * @returns {Promise} A promise
+	 */
+	requestFactory('PUT', target)
 
 /**
  * Prepares a post method
  *
- * @param {String} target The call target
- * @returns {Function} The request method
+ * @param {string} target The call target
+ * @returns {function} The request method
  * @private
  */
 const _destroy = target =>
-  /**
-   * Performs a delete request
-   *
-   * @param {Object} params The params object
-   * @param {*} id The optional request id
-   * @returns {Promise<any>} A promise
-   */
-  requestFactory("DELETE", target);
+	/**
+	 * Performs a delete request
+	 *
+	 * @param {*} id The optional request id
+	 * @param {object} params The params object
+	 * @returns {Promise} A promise
+	 */
+	requestFactory('DELETE', target)
 
 /**
  * Get's all possible methods for a target
  *
- * @param {String} target The target name (eg: collections)
- * @returns {{get: Function, post: Function, put: Function, destroy: Function}} The methods object
+ * @param {string} target The target name (eg: collections)
+ * @returns {{get: function, post: function, put: function, destroy: function}} The methods object
  */
 const getMethods = target => ({
-  get: _get(target),
-  post: _post(target),
-  put: _put(target),
-  destroy: _destroy(target)
-});
+	get: _get(target),
+	post: _post(target),
+	put: _put(target),
+	destroy: _destroy(target)
+})
 
 export default {
-  Collections: getMethods("collections"),
-  Environments: getMethods("environments"),
-  Mocks: getMethods("mocks"),
-  Monitors: getMethods("monitors"),
-  User: getMethods("user")
-};
+	Collections: getMethods('collections'),
+	Environments: getMethods('environments'),
+	Mocks: getMethods('mocks'),
+	Monitors: getMethods('monitors'),
+	User: getMethods('user'),
+	Builder: { collection, environment }
+}
